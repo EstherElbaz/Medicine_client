01dfb1fa15d695048414aac7a0c8f16fa3ffb555
MedicationsManagger <strmcl123@gmail.com>
2023-06-11 13:53:48 +0300

    update contextyyyll

--- src/components/MyMedicines.tsx
+++ src/components/MyMedicines.tsx
@@ -1,20 +1,35 @@
 import userEvent from "@testing-library/user-event";
 import React from "react";
-import { useEffect, useState ,useContext} from "react";
-import { User } from "../models";
+import { useEffect, useState, useContext } from "react";
+import { MedicineForUser, User } from "../models";
 import AddMedicine from "./AddMedicine";
 import MedicineToShow from "./MedicineToShow";
-import UserContext from "./User/UserContext";
+import { useUserContext } from "./User/UserContext";
 
 export default function MyMedicines() {
 
-    const [medicines, setMedicines] = useState([]);
+    const [medicines, setMedicines] = useState<any>([]);
     const [addMed, setAddMed] = useState(false);
-    const { user: us } = useContext(UserContext)
-    const getMedicinesForUser = async (userId: Number) => {
+    const { user:yyy } = useUserContext()
 
+    console.log(yyy, "user from context");
 
-        const res = await fetch(`https://localhost:7247/api/MedicineForUser?userId=${userId}`);
+    const getMedicinesForUser = async () => {
+        // function compare(a: MedicineForUser, b: MedicineForUser) {
+        //     if (a.Hour < b.Hour) {
+        //         return -1;
+        //     }
+        //     if (a.Hour > b.Hour) {
+        //         return 1;
+        //     }
+        //     return 0;
+        // }
+
+        console.log(yyy?.id, "jjj");
+        const tr=localStorage.getItem("userId")
+        const res = await fetch(`https://localhost:7247/api/MedicineForUser?userId=${tr}`);
+        console.log(res);
+
         if (!res.ok) {
             throw console.error(`error: stautus code is ${res.status}`);
         }
@@ -22,25 +37,40 @@ export default function MyMedicines() {
             alert("לא קיימות תרופות");
         }
         else {
+
             const medicinesList = await res.json();
-            await setMedicines(medicinesList);
+            var medicinesListByStusus = [];
+            for (var e in medicinesList) {
+
+                //update to filter insted of for loop
+                if (medicinesList[e].status) {
+                    medicinesListByStusus.push(medicinesList[e]);
+                }
+            }
+            const filteredAndOrderedArray = medicinesList
+                .filter((item: { status: Boolean; }) => item.status == true)
+                /*.sort((a, b) => a.ts - b.ts);*/
+
+
+            medicinesListByStusus.sort(compare);
+            await setMedicines(medicinesListByStusus);
             console.log(medicines);
         }
     }
 
+
     useEffect(() => {
-        let u = (sessionStorage.getItem("userId"));
-        let user: User;
-        if (u != null) {
-            user = JSON.parse(u);
-            //להגדיר את הסטטי של יוזר ניים ויוזר אי די 
-            getMedicinesForUser(user.id);
-        }
-        else {
-            alert("משתמש לא מחובר")
-        }
+        // let u = (sessionStorage.getItem("user")); /*user.id to use usercontext*/
+        // let user: User;
+        // if (user?.id != null) {
+        // user = JSON.parse(u);
+        getMedicinesForUser();
+        // }
+        // else {
+        //     alert("משתמש לא מחובר")
+        // }
     }, []);
-    
+
     return (
         <div>
             <h1> רשימת התרופות למשתמש</h1>
@@ -48,7 +78,7 @@ export default function MyMedicines() {
                 {medicines.map((med: any) => {
                     return (
                         <div>
-                            <MedicineToShow name={med.medicine} note={med.note} hour={med.hour} id={med.id}></MedicineToShow>
+                            <MedicineToShow name={med.name} note={med.note} hour={med.hour} id={med.id}></MedicineToShow>
                         </div>
                     );
                 })}
