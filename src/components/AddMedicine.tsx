f0849ff2e9cbbde3e811d6623c8e5e0ff5e96d09
משתמש זה < @DESKTOP-SN2JHFI>
2023-06-15 22:56:46 +0300

    medicine list from an external db

--- src/components/AddMedicine.tsx
+++ src/components/AddMedicine.tsx
@@ -2,10 +2,10 @@ import { Autocomplete, TextField } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
 import { MedicineForUser, User } from "../models";
 import { useUserContext } from "./User/UserContext";
+import { ifError } from "assert";
 
 export default function AddMedicine(props: any) {
 
-  
   const { user: us } = useUserContext();
   const [medId, setMedId] = useState(1);
   const [hour, setHour] = useState<Number>(12);
@@ -14,39 +14,54 @@ export default function AddMedicine(props: any) {
   const [note, setNote] = useState<String>("");
   const [status, setStutus] = useState<Boolean>(true);
   const [MedicineName, setMedicineName] = useState<String>("");
-  const [userId,setUserId] = useState<any>();
+  const [userId, setUserId] = useState<any>();
 
-  console.log(us);
+  const [medList, setMedList] = useState([]);
 
-  const opt = [
-    { value: 1, label: "אקמול" },
-    { value: 2, label: "ריטלין" },
-    { value: 89, label: "אוקלפן" },
-    { value: 92, label: "ריגוומין" },
-    { value: 19, label: "אופטלגין" },
-    { value: 116, label: "מוקסיפן" },
-    { value: 15, label: "אדויל" },
-    { value: 14, label: "רפאפן" },
-  ];
 
-  const fetchMedFromApi = () => { };
+
+  const setMedArr = (medRes: any) => {
+    let medObgs: any = [];
+    medRes.forEach((element: any) => {
+      console.log(element.name, "name");
+      console.log(element.id, "id");
+      const medObj = {
+        value: element.id,
+        label: element.name
+      }
+      medObgs.push(medObj);
+    });
+
+    setMedList(medObgs);
+  }
+
+  const fetchMedFromApi = async () => {
+    const res = await fetch(`https://localhost:7247/api/Medicine`);
+    if (!(res).ok) {
+      throw Error(`status: ${res.status} is making troubles again😱`);
+    }
+    else
+      if (res.status === 204) {
+        alert("לא קיימות תרופות")
+      }
+      else {
+        const medicines = await res.json();
+        setMedList(medicines);
+        console.log(medicines, "med state")
+        setMedArr(medicines);
+
+      }
+  };
 
   const addMedForUser = async () => {
-    
+
     let medicineForUser = new MedicineForUser(
-     /* us?.id,*/
-     userId,
-      medId,
-      sumOfPills,
-      name,
-      hour,
-      note,
-      status,
-      MedicineName
-    );
-    console.log(medicineForUser);
+      /* us?.id,*/userId, medId, sumOfPills, name, hour, note, status, MedicineName);
+
     await addData(medicineForUser);
   };
+
+
   const addData = async (medToAdd: MedicineForUser) => {
     let medJson = JSON.stringify(medToAdd);
 
@@ -71,54 +86,35 @@ export default function AddMedicine(props: any) {
 
   useEffect(() => {
     setUserId(localStorage.getItem("userId"));
-    //let u = sessionStorage.getItem("user");
-    // let user: User;
-    // if (u != null) {
-    //   user = JSON.parse(u);
-    // } else {
-    //   alert("משתמש לא מחובר");
-    // }
-  },[]);
+    fetchMedFromApi();
+
+  }, []);
 
   return (
     <div>
-      <h1>רשימת התרופות ל{us?.firstName}</h1>
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={opt}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="תרופה" />}
-      ></Autocomplete>
+      <Autocomplete disablePortal id="combo-box-demo" options={medList} sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="תרופה" />}></Autocomplete>
       <input
-        id="note"
-        placeholder="הערה "
-        onChange={(e) => {
+        id="note" placeholder="הערה " onChange={(e) => {
           {
             setNote(e.target.value);
           }
-        }}
-      ></input>
-      <input
-        id="sumofpill"
-        type="Number"
-        placeholder="כמות "
-        onChange={(e) => {
-          {
-            setSumOfPills(Number(e.target.value));
-          }
-        }}
-      ></input>
-      <input
-        id="hour"
-        type="Number"
-        placeholder="שעה"
-        onChange={(e) => {
-          {
-            setHour(Number(e.target.value));
-          }
-        }}
-      ></input>
+        }}>
+
+      </input>
+      <input id="sumofpill" type="Number" placeholder="כמות " onChange={(e) => {
+        {
+          setSumOfPills(Number(e.target.value));
+        }
+      }}>
+      </input>
+      <input id="hour" type="Number" placeholder="שעה" onChange={(e) => {
+        {
+          setHour(Number(e.target.value));
+        }
+      }}>
+
+      </input>
       <button onClick={() => addMedForUser()}>הוסף תרופה</button>
     </div>
   );
